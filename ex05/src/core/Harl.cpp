614597d13445c80ecb9b409022a386e6e3bedb3a
willtrigo <willtrigo@gmail.com>
2025-03-07 21:45:04 -0300

    Fix: add white space in each complain  Changes to be committed: 	modified:   ../ex05/src/core/Harl.cpp

--- ex05/src/core/Harl.cpp
+++ ex05/src/core/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 00:07:30 by dande-je          #+#    #+#             */
-/*   Updated: 2025/03/07 21:16:34 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/03/07 21:44:45 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,19 @@ Harl& Harl::operator=(const Harl& other) {
 Harl::~Harl() {}
 
 void Harl::debug(void) {
-    std::cout << m_color.setColor(BG_RESET, BLUE, "[DEBUG] ") << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+    std::cout << m_color.setColor(BG_RESET, BLUE, "[ DEBUG ] ") << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info(void) {
-    std::cout << m_color.setColor(BG_RESET, YELLOW, "[INFO] ") << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+    std::cout << m_color.setColor(BG_RESET, YELLOW, "[ INFO ] ") << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void) {
-    std::cout << m_color.setColor(BG_RESET, ORANGE, "[WARNING] ") << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << m_color.setColor(BG_RESET, ORANGE, "[ WARNING ] ") << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(void) {
-    std::cout << m_color.setColor(BG_RESET, RED, "[ERROR] ") << "This is unacceptable! I want to speak to the manager now." << std::endl;
+    std::cout << m_color.setColor(BG_RESET, RED, "[ ERROR ] ") << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 bool Harl::harlCheckArgs(int argc, char* argv[]) {
