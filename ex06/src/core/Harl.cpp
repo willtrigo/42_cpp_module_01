e75f2e7b2ed0d77a11a157813110cba4ce7e598c
willtrigo <willtrigo@gmail.com>
2025-03-07 21:41:43 -0300

    Feat: implement logic of ex06  Changes to be committed: 	new file:   Makefile 	new file:   src/core/Harl.cpp 	new file:   src/core/Harl.hpp 	new file:   src/main/main.cpp 	new file:   src/utils/TerminalColor.cpp 	new file:   src/utils/TerminalColor.hpp

---
+++ ex06/src/core/Harl.cpp
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 00:07:30 by dande-je          #+#    #+#             */
+/*   Updated: 2025/03/07 21:39:16 by dande-je         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "core/Harl.hpp"
+#include "utils/TerminalColor.hpp"
+#include <iostream>
+
+const Harl::Level Harl::levelMap[TOTAL_LEVEL] = {
+  {"DEBUG", &Harl::debug},
+  {"INFO", &Harl::info},
+  {"WARNING", &Harl::warning},
+  {"ERROR", &Harl::error},
+  {"INVALID", &Harl::invalid}
+};
+
+Harl::Harl() : m_color(TerminalColor::getInstance()) {}
+
+Harl::Harl(const Harl& other) : m_color(TerminalColor::getInstance()) {
+  (void)other;
+  std::cout << "Copy constructor called for Hald" << std::endl;
+}
+
+Harl& Harl::operator=(const Harl& other) {
+  if (this != &other) {}
+  std::cout << "Copy assignment operator called for Harl" << std::endl;
+  return *this;
+}
+
+Harl::~Harl() {}
+
+void Harl::debug(void) {
+    std::cout << m_color.setColor(BG_RESET, BLUE, "[DEBUG] ") << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(void) {
+    std::cout << m_color.setColor(BG_RESET, YELLOW, "[INFO] ") << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning(void) {
+    std::cout << m_color.setColor(BG_RESET, ORANGE, "[WARNING] ") << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error(void) {
+    std::cout << m_color.setColor(BG_RESET, RED, "[ERROR] ") << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::invalid(void) {
+    std::cout << m_color.setColor(BG_WHITE, BLACK, "[Probably complaining about insignificant problems]") << std::endl;
+}
+
+bool Harl::harlCheckArgs(int argc, char* argv[]) {
+  if (argc != TOTAL_ARGS) {
+    std::cout << "You have this levels:" << std::endl;
+    for (int i = 0; i < INVALID; ++i) {
+      std::cout << "  " << this->levelMap[i].levelName << std::endl;
+    }
+    std::cout << "Usage: " + std::string(argv[PROGRAM_NAME]) + " <level>" << std::endl;
+    return false;
+  } 
+  return true;
+}
+
+void Harl::harlFilter(std::string level) {
+  for (int i = 0; i < INVALID; ++i) {
+    if (this->levelMap[i].levelName == level) {
+      complain(level);
+      return;
+    }
+  }
+  complain(this->levelMap[INVALID].levelName);
+}
+
+void Harl::complain(std::string level) {
+  for (int i = 0; i < TOTAL_LEVEL; ++i) {
+    if (this->levelMap[i].levelName == level) {
+      (this->*levelMap[i].func)();
+      return;
+    }
+  }
+}
---
+++ ex06/src/core/Harl.hpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 00:07:47 by dande-je          #+#    #+#             */
+/*   Updated: 2025/03/07 21:34:51 by dande-je         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include "utils/TerminalColor.hpp"
+#include <string>
+
+enum LevelEnum {
+  DEBUG,
+  INFO,
+  WARNING,
+  ERROR,
+  INVALID,
+  TOTAL_LEVEL
+};
+
+class Harl {
+ private:
+  enum Arguments {
+    PROGRAM_NAME,
+    LEVEL,
+    TOTAL_ARGS
+  };
+  typedef void (Harl::* harlFunc)();
+  struct Level {
+    std::string levelName;
+    harlFunc func;
+  };
+  static const Level levelMap[TOTAL_LEVEL];
+  const TerminalColor& m_color;
+
+  void debug(void);
+  void info(void);
+  void warning(void);
+  void error(void);
+  void invalid(void);
+  void complain(std::string level);
+ public:
+  Harl();
+  Harl(const Harl& other);
+  Harl& operator=(const Harl& other);
+  ~Harl();
+
+  bool harlCheckArgs(int argc, char* argv[]);
+  void harlFilter(std::string level);
+};
+
+#endif // HARL_HPP
