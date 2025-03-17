a95185c031fb8ef83289e8172fcc933f04e67074
willtrigo <willtrigo@gmail.com>
2025-03-17 00:47:12 -0300

    Fix: add new line  Changes to be committed: 	modified:   ../ex02/src/main/main.cpp

--- ex02/src/main/main.cpp
+++ ex02/src/main/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 22:28:15 by dande-je          #+#    #+#             */
-/*   Updated: 2025/03/03 22:41:37 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/03/17 00:47:05 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int main() {
   const std::string str("HI THIS IS BRAIN");
   const std::string* stringPTR = &str;
   const std::string& stringREF = str;
+
 	std::cout << "The memory address of the string variable " << &str << std::endl;
 	std::cout << "The memory address held by stringPTR " << stringPTR << std::endl;
 	std::cout << "The memory address held by stringREF " << &stringREF << std::endl;
